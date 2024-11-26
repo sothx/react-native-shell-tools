886395e9ce076467e439e493ff463b8f27537cec
sothx <i@sothx.com>
2024-11-27 05:20:06 +0800

    fix(args): fix shellCommand text

--- src/NativeReactNativeShellTools.ts
+++ src/NativeReactNativeShellTools.ts
@@ -3,8 +3,8 @@ import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
   multiply(a: number, b: number): number;
-  execAsyncCommand(shellCommon: string): Promise<string>;
-  execCommand(shellCommon: string): string;
+  execAsyncCommand(shellCommand: string): Promise<string>;
+  execCommand(shellCommand: string): string;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('ReactNativeShellTools');
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ export function multiply(a: number, b: number): number {
   return ReactNativeShellTools.multiply(a, b);
 }
 
-export function execCommand(shellCommon: string): String {
-  return ReactNativeShellTools.execCommand(shellCommon);
+export function execCommand(shellCommand: string): String {
+  return ReactNativeShellTools.execCommand(shellCommand);
 }
 
-export function execAsyncCommand(shellCommon: string): Promise<string> {
-  return ReactNativeShellTools.execAsyncCommand(shellCommon);
+export function execAsyncCommand(shellCommand: string): Promise<string> {
+  return ReactNativeShellTools.execAsyncCommand(shellCommand);
 }
